53cc6c809a7a17f9377419799e8c5cf2cbdcd7d2
Flynn <flynn.whelehan@gmail.com>
2023-02-05 11:47:13 +0000

    Question outlines

---
+++ exercises/ex3.hs
@@ -0,0 +1,17 @@
+{-
+Exercises
+1. Using list comprehension, give an expression that calculates the sum of
+the first one hundred integer squares:
+1^2 + 2^2 + 3^2 + … + 100^2
+-}
+
+
+{-
+2. The library function
+'replicate :: Int → a → [a]'
+produces a list whose length is given by the first argument and whose
+elements are identical to the second argument. For example,
+replicate 3 True = [True,True,True]
+
+Use list comprehension to define replicate.
+-}
