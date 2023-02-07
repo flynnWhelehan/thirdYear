44c04f4ae1a0985c583fbadcfefea2acb43e9e94
Flynn <flynn.whelehan@gmail.com>
2023-02-07 13:44:13 +0000

    Rest of solutions

--- functionalProgramming/labs/lab3.hs
+++ functionalProgramming/labs/lab3.hs
@@ -45,6 +45,8 @@ ponents ai in [1..n]. For example:
 > pyths 10
 [(3,4,5)], (4,3,5), (6,8,10), (8,6,10)]
 -}
+pyths :: Int -> [(Int,Int,Int)]
+pyths n = [(x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
 
 {-
 4. A positive integer is perfect if it equals the sum of all of its factors, ex-
@@ -56,4 +58,7 @@ the list of all perfect numbers up to n. For example:
 (Hint: Use the library function init and the function factors as
 defined in the lecture slides VI.)
 -}
-
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], let fs = factors x, not (null fs), sum fs == x]
+    where factors x = [y | y <- [1..x-1], x `mod` y == 0]
+{-The not (null fs) check ensures that the list of factors fs is not empty before the init function is called on it.-}
