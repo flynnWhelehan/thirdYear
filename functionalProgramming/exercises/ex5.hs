ec91ba7cda1159c3e6152ae5c02beb6b69d2854f
Flynn <flynn.whelehan@gmail.com>
2023-02-13 23:15:33 +0000

    All complete
    Was quite short compared to labs.

--- functionalProgramming/exercises/ex5.hs
+++ functionalProgramming/exercises/ex5.hs
@@ -3,14 +3,18 @@
 a. all :: (a→Bool) → [Bool] → Bool, which decides if all elements of a
 list satisfy a predicate.
 -}
-
+all p = and . map p
 {-
 b. takeWhile :: (a→Bool) → [a] → [a], which selects elements from a
 list while they satisfy a predicate.
 -}
-
-
+takeWhile' _ [] = []
+takeWhile' p (x:xs) 
+    | p x = x : takeWhile' p xs
+    | otherwise = x:xs
 {-
 2. Without looking at the Prelude, define the function curry that converts a
 function on pairs into a curried function.
 -}
+curry :: ((a,b) -> c) -> (a -> b -> c)
+curry f = \x y -> f (x,y)
