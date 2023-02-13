9451b4ae2c2ba1d3d2defa1dea551612473dd87c
Flynn <flynn.whelehan@gmail.com>
2023-02-13 21:43:43 +0000

    Q2 complete

--- functionalProgramming/labs/lab4.hs
+++ functionalProgramming/labs/lab4.hs
@@ -13,4 +13,13 @@ replicate' n item = [item] ++ replicate' (n-1) item
 elem' :: Eq a => a -> [a] -> Bool
 elem' val [] = False
 elem' val (x:xs) = (val == x) || elem' val xs
---elem' val xs = foldr (\ x -> (||) (val == x)) True xs
+-- also can be done like this remember 
+    -- elem' val xs = foldr (\ x -> (||) (val == x)) True xs
+
+-- 2. Recursive function that merges two sorted lists of integers to give a single sorted list.
+merge' :: [Int] -> [Int] -> [Int]
+merge' [] ys = ys
+merge' xs [] = xs
+merge' (x:xs) (y:ys) = if x <= y 
+    then [x] ++ merge' xs (y:ys) 
+    else [y] ++ merge' (x:xs) ys 
