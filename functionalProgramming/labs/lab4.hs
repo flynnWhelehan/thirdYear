0987bba1dcb5fddb9732d25d4a620a2794765aed
Flynn <flynn.whelehan@gmail.com>
2023-02-13 22:33:12 +0000

    Q3 complete

--- functionalProgramming/labs/lab4.hs
+++ functionalProgramming/labs/lab4.hs
@@ -23,3 +23,21 @@ merge' xs [] = xs
 merge' (x:xs) (y:ys) = if x <= y 
     then [x] ++ merge' xs (y:ys) 
     else [y] ++ merge' (x:xs) ys 
+
+-- 3. Recursive function for merge sort, with rules: 
+    -- lists of length <= to 1 are already sorted
+    -- other lists can be sorted by sorting the two halves and merging the resulting lists
+--also make use of halve:
+halve' :: [a] -> ([a], [a])
+halve' xs = splitAt (length xs `div` 2) xs
+
+msort' :: [Int] -> [Int]
+msort' xs
+    | length xs <= 1 = xs
+    | otherwise = merge' (msort' left) (msort' right)
+    where (left, right) = halve' xs
+
+
+
+
+
