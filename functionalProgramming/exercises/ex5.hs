cc864eda351a392e2a46dcd04264db8290beb42c
Flynn <flynn.whelehan@gmail.com>
2023-02-13 23:05:50 +0000

    Init

---
+++ functionalProgramming/exercises/ex5.hs
@@ -0,0 +1,16 @@
+-- 1. Without looking at the Prelude, define the following functions
+{-
+a. all :: (a→Bool) → [Bool] → Bool, which decides if all elements of a
+list satisfy a predicate.
+-}
+
+{-
+b. takeWhile :: (a→Bool) → [a] → [a], which selects elements from a
+list while they satisfy a predicate.
+-}
+
+
+{-
+2. Without looking at the Prelude, define the function curry that converts a
+function on pairs into a curried function.
+-}
