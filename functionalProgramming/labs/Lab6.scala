fde27cca0a9cf952289c552ee78eb503a2a1a640
Flynn <flynn.whelehan@gmail.com>
2023-03-02 12:55:40 +0000

    Q2 Complete

--- functionalProgramming/labs/Lab6.scala
+++ functionalProgramming/labs/Lab6.scala
@@ -26,28 +26,29 @@ class Executive(name: String, salary: Int, department: String) extends Manager(n
   override def toString: String = s"Executive[name=$name, department=$department, salary=$salary]"
 end Executive
 
-/* Q1 
-(d) Supply a test program that tests these classes and methods.
-*/
-@main def Lab6Main =
-  val e = Employee("Flynn", 24000)
-  println(e)
-  val m = Manager("Flynn", 24000, "IT")
-  println(m)
-  val x = Executive("Flynn", 24000, "IT")
-  println(x)
-
 /* Q2
 Define the following functions in Scala and use its interpreter to test
 that each of them is well-defined.
+
 (a) factors : (n:Int)List[Int] that, for any n, returns the list
 of factors of n.
+(Hint: Use for-comprehension to define (2a) and (2c).)
+
 (b) prime : (n:Int)Boolean that decides whether the input is a
 prime number.
+
 (c) primes : (n:Int)List[Int] that, for any n, returns the list of
 the prime numbers that are less than or equal to n.
 (Hint: Use for-comprehension to define (2a) and (2c).)
 */
+class Functions():
+  def factors(n: Int): List[Int] =
+    (for i <- 1 to n if n % i == 0 yield i).toList
+  def prime(n: Int): Boolean = 
+    factors(n).length == 2 
+  def primes(n: Int): List[Int] =
+    (for i <- 1 to n if prime(i) yield i).toList
+end Functions
 
 /* Q3
 Write a program that prompts the user for an integer, reads it, and
@@ -55,3 +56,21 @@ then prints out all prime numbers up to that integer. (Hint: You
 may use readInt and println to read an integer and print a line,
 respectively.)
 */
+
+
+@main def Lab6Main =
+  // Q1
+  val e = Employee("Flynn", 24000)
+  println(e)
+  val m = Manager("Flynn", 24000, "IT")
+  println(m)
+  val x = Executive("Flynn", 24000, "IT")
+  println(x)
+
+  // Q2
+  val f = Functions()
+  println(f.factors(10))
+  println(f.prime(10))
+  println(f.primes(10))
+  
+  // Q3
